bf891b39ff86493735b64f99a6bcf19071c7b766
human-sketch <73455299+human-sketch@users.noreply.github.com>
2021-04-09 16:02:49 +1200

    Motor Drive
    Added Motor Drive

--- Sample Project-2021-03-03T23-37-39/src/main.cpp
+++ Sample Project-2021-03-03T23-37-39/src/main.cpp
@@ -13,6 +13,22 @@
 #include "vex.h"
 
 using namespace vex;
+controller Controller1;
+
+motor Ldrive(PORT1, ratio18_1);
+motor Rdrive(PORT10, ratio18_1);
+
+void MoveMotors(float Iinput, float rInput) {
+  Ldrive.spin(fwd, Iinput, pct);
+  Rdrive.spin(fwd, rInput, pct);
+}
+
+void usercontrol(void)  {
+  while (true) {
+    MoveMotors(Controller1.Axis3.value(), Controller1.Axis2.value());
+  }
+
+}
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
